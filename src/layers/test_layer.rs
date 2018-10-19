e7b86a399bf6fe0c8888bced6a120295506fae77
Daniel Booth <dpbooth@uchicago.edu>
2018-10-19 00:20:43 -0400

    Map + Render testing

--- src/layers/test_layer.rs
+++ src/layers/test_layer.rs
@@ -13,15 +13,18 @@ use systems::TestSystem;
 pub struct TestLayer{
     dispatcher: specs::Dispatcher<'static, 'static>,
     image: graphics::Image,
+    map: graphics::Image,
 }
 
 impl TestLayer {
     pub fn new(ggez_ctx: &mut ggez::Context, world: &mut World) -> Self {
         let dispatcher = Self::register_systems();
         let image = graphics::Image::new(ggez_ctx,"/tile.png").unwrap();
+        let map = graphics::Image::new(ggez_ctx,"/test_map.jpg").unwrap();
         TestLayer {
             dispatcher,
             image,
+            map,
         }   
     }
     fn register_systems() -> specs::Dispatcher<'static, 'static>{
@@ -43,9 +46,22 @@ impl Layer<World, i32> for TestLayer {
         graphics::clear(ggez_ctx);
         graphics::draw_ex(
             ggez_ctx,
-            &self.image,
+            &self.map,
             graphics::DrawParam{
                 dest: graphics::Point2::new(0.0,0.0),
+                //will use to scale objects relative to map.
+                //maps themselves *should* maintain native resolution butwe couuld add support
+                scale: graphics::Point2::new(1.0,1.0),
+                ..Default::default()
+            },
+        )?;
+        graphics::draw_ex(
+            ggez_ctx,
+            &self.image,
+            graphics::DrawParam{
+                // Use with camera to crop things half on camera
+                src: graphics::Rect::new(0.1,0.1,0.5,0.5),
+                dest: graphics::Point2::new(50.0,50.0),
                 ..Default::default()
             },
         )?;
