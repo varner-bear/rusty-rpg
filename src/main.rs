34945fb9afe25f54701872cba026256f63f88fb3
Daniel Booth <dpbooth@uchicago.edu>
2019-04-20 17:25:25 -0400

    Working Square + Text

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use quicksilver::prelude::*;
 struct Game {
     // defines some asset for the game, which is a future of an image
     asset: Asset<Image>,
+    font_test: Asset<Image>,
 }
 
 impl State for Game {
@@ -12,7 +13,15 @@ impl State for Game {
     fn new() -> Result<Self> {
         // loads the image in the background while the show goes on
         let asset = Asset::new(Image::load("test_map.jpg"));
-        Ok(Game{asset})
+        let font_mononoki = "mononoki-Regular.ttf";
+
+        let font_test = Asset::new(Font::load(font_mononoki).and_then(|font|{
+            font.render(
+                "Test",&FontStyle::new(20.0, Color::BLACK),
+            )
+        }));
+
+        Ok(Game{asset,font_test})
     }
 
     // Process keyboard + mouse, updates the game state
@@ -23,11 +32,23 @@ impl State for Game {
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         // clears the window
         window.clear(Color::WHITE)?;
+        
         // draws the image if it is loaded, keeps going if it doesn't
         self.asset.execute(|image| {
             window.draw(&image.area().with_center((400,300)),Img(&image));
             Ok(())
-        })
+        })?;
+
+        window.draw_ex(&Rectangle::new((100,100), (32,32)), Col(Color::BLUE), Transform::rotate(45),1);
+        self.font_test.execute(|image| {
+            window.draw(
+                &image.area().translate((2, 10)),
+                Img(&image),
+            );
+            Ok(())
+        })?;
+        //println!("do we ever get here");
+        Ok(())
     }
 }
 
